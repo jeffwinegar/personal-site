b308d83a1ecb7f329e93da632669be5d81399a1f
Jeff Winegar <winegarj@gmail.com>
2021-12-08 21:13:49 -0800

    feat: add back link

--- src/pages/{mdx.slug}.js
+++ src/pages/{mdx.slug}.js
@@ -2,12 +2,34 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-import SyntaxStyles from "../components/styles/SyntaxHighlightingStyles"
+import styled from "styled-components"
 
+import SyntaxStyles from "../components/styles/SyntaxHighlightingStyles"
 import SEO from "../components/SEO"
 import PostMeta from "../components/PostMeta"
 
 const shortcodes = { Link }
+
+const StyledBackLink = styled(Link)`
+  display: inline-block;
+  font-size: 1rem;
+  line-height: 1.25;
+  padding-left: 1rem;
+  position: relative;
+
+  &::before {
+    content: "←";
+    left: 0;
+    position: absolute;
+    top: 50%;
+    transform: translate3d(0, -50%, 0);
+    transition: transform 0.25s ease;
+  }
+  &:focus::before,
+  &:hover::before {
+    transform: translate3d(-15%, -50%, 0);
+  }
+`
 
 export const pageQuery = graphql`
   query GetPost($id: String) {
@@ -41,6 +63,9 @@ const PostLayout = ({ data: { mdx: post } }) => {
       />
       <SyntaxStyles />
 
+      <div>
+        <StyledBackLink to="/#some-thoughts">Back</StyledBackLink>
+      </div>
       <header>
         <h1>{post.frontmatter.title}</h1>
         <PostMeta
