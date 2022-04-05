c4baa7252c5cfd61dbc29f8c8d3f3357dcd75ce4
Jeff Winegar <11320804+jeffwinegar@users.noreply.github.com>
2022-04-04 19:31:07 -0700

    fix: light theme flash on load in dark mode (#38)
    * fix: light theme flash on load in dark mode
    
    * wip: inject theme script html build
    
    * fix: typo of setPreBodyComponents
    
    * feat: set body attribute to initial theme
    
    * refactor: parse localStorage and check for auto preference
    
    * refactor: cleanup console.log and extract function out of string

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,44 @@
-const React = require("react")
-const Layout = require("./src/components/Layout").default
+import React from "react"
+import Layout from "./src/components/Layout"
 
-exports.wrapPageElement = ({ element, props }) => {
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
+function setTheme() {
+  function getInitialColorScheme() {
+    // Check for localStorage value
+    const persistedColorSchemePreference = JSON.parse(
+      window.localStorage.getItem("jw:setting:preferredColorScheme")
+    )
+    const hasPersistedPreference =
+      typeof persistedColorSchemePreference === "string"
+
+    if (hasPersistedPreference && persistedColorSchemePreference !== "auto")
+      return persistedColorSchemePreference
+
+    // if no localStorage value or value is 'auto' check media query
+    const mql = window.matchMedia("(prefers-color-scheme: dark)")
+    const hasMediaQueryPreference = typeof mql.matches === "boolean"
+
+    if (hasMediaQueryPreference || persistedColorSchemePreference === "auto")
+      return mql.matches ? "dark" : "light"
+
+    // if browser doesn't support color themes set default to 'light'
+    return "light"
+  }
+
+  const colorScheme = getInitialColorScheme()
+
+  document.body.setAttribute("data-theme", colorScheme)
+}
+
+const ThemeScriptTag = () => {
+  const clientSideCode = `(${String(setTheme)})()`
+
+  return <script dangerouslySetInnerHTML={{ __html: clientSideCode }} />
+}
+
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents(<ThemeScriptTag />)
+}
+
+export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 }
