aff567045c6ce00138c7a20cd855a2b8d67d7269
Jeff Winegar <winegarj@gmail.com>
2022-04-04 19:14:21 -0700

    refactor: cleanup console.log and extract function out of string

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,35 +1,36 @@
 import React from "react"
 import Layout from "./src/components/Layout"
 
+function setTheme() {
+  function getInitialColorScheme() {
+    // Check for localStorage value
+    const persistedColorSchemePreference = JSON.parse(
+      window.localStorage.getItem("jw:setting:preferredColorScheme")
+    )
+    const hasPersistedPreference =
+      typeof persistedColorSchemePreference === "string"
+
+    if (hasPersistedPreference && persistedColorSchemePreference !== "auto")
+      return persistedColorSchemePreference
+
+    // if no localStorage value or value is 'auto' check media query
+    const mql = window.matchMedia("(prefers-color-scheme: dark)")
+    const hasMediaQueryPreference = typeof mql.matches === "boolean"
+
+    if (hasMediaQueryPreference || persistedColorSchemePreference === "auto")
+      return mql.matches ? "dark" : "light"
+
+    // if browser doesn't support color themes set default to 'light'
+    return "light"
+  }
+
+  const colorScheme = getInitialColorScheme()
+
+  document.body.setAttribute("data-theme", colorScheme)
+}
+
 const ThemeScriptTag = () => {
-  const clientSideCode = `(function () {
-    function getInitialColorScheme() {
-      // Check for localStorage value
-      const persistedColorSchemePreference = JSON.parse(
-        window.localStorage.getItem("jw:setting:preferredColorScheme")
-      )
-      const hasPersistedPreference =
-        typeof persistedColorSchemePreference === "string"
-
-      if (hasPersistedPreference && persistedColorSchemePreference !== "auto")
-        return persistedColorSchemePreference
-
-      // if no localStorage value or value is 'auto' check media query
-      const mql = window.matchMedia("(prefers-color-scheme: dark)")
-      const hasMediaQueryPreference = typeof mql.matches === "boolean"
-
-      if (hasMediaQueryPreference || persistedColorSchemePreference === "auto")
-        return mql.matches ? "dark" : "light"
-
-      // if browser doesn't support color themes set default to 'light'
-      return "light"
-    }
-
-    console.log(getInitialColorScheme())
-    const colorMode = getInitialColorScheme()
-
-    document.body.setAttribute("data-theme", colorMode)
-  })()`
+  const clientSideCode = `(${String(setTheme)})()`
 
   return <script dangerouslySetInnerHTML={{ __html: clientSideCode }} />
 }
