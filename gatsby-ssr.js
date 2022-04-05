41a2a177db8e722d06e7488b7582339e0a0e2ffa
Jeff Winegar <winegarj@gmail.com>
2022-04-04 18:00:04 -0700

    wip: inject theme script html build

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,29 +1,37 @@
 import React from "react"
 import Layout from "./src/components/Layout"
 
-const getInitialColorScheme = () => {
-  // Check for localStorage value
-  const persistedColorSchemePreference = window.localStorage.getItem(
-    "jw:setting:preferredColorScheme"
-  )
-  const hasPersistedPreference =
-    typeof persistedColorSchemePreference === "string"
+const ThemeScriptTag = () => {
+  const clientSideCode = `(function() {
+    function getInitialColorScheme() {
+      // Check for localStorage value
+      const persistedColorSchemePreference = window.localStorage.getItem(
+        "jw:setting:preferredColorScheme"
+      )
+      const hasPersistedPreference =
+        typeof persistedColorSchemePreference === "string"
+    
+      if (hasPersistedPreference && persistedColorSchemePreference !== "auto")
+        return persistedColorSchemePreference
+    
+      // if no localStorage value or value is 'auto' check media query
+      const mql = window.matchMedia("(prefers-color-scheme: dark)")
+      const hasMediaQueryPreference = typeof mql.matches === "boolean"
+    
+      if (hasMediaQueryPreference) return mql.matches ? "dark" : "light"
+    
+      // if browser doesn't support color themes set default to 'light'
+      return "light"
+    }
 
-  if (hasPersistedPreference && persistedColorSchemePreference !== "auto")
-    return persistedColorSchemePreference
+    console.log(getInitialColorScheme);
+  })()`
 
-  // if no localStorage value or value is 'auto' check media query
-  const mql = window.matchMedia("(prefers-color-scheme: dark)")
-  const hasMediaQueryPreference = typeof mql.matches === "boolean"
-
-  if (hasMediaQueryPreference) return mql.matches ? "dark" : "light"
-
-  // if browser doesn't support color themes set default to 'light'
-  return "light"
+  return <script dangerouslySetInnerHTML={{ __html: clientSideCode }} />
 }
 
-export const onRenderBody = ({ setBodyAttributes }) => {
-  setBodyAttributes({ "data-theme": getInitialColorScheme })
+export const onRenderBody = ({ setPreBodyComponent }) => {
+  setPreBodyComponent(<ThemeScriptTag />)
 }
 
 export const wrapPageElement = ({ element, props }) => {
