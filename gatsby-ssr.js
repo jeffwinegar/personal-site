c3da278d5eb13f07fd8d26b8ed28c0709ea2fb4b
Jeff Winegar <winegarj@gmail.com>
2022-04-04 18:17:37 -0700

    feat: set body attribute to initial theme

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "./src/components/Layout"
 
 const ThemeScriptTag = () => {
-  const clientSideCode = `(function() {
+  const clientSideCode = `(function () {
     function getInitialColorScheme() {
       // Check for localStorage value
       const persistedColorSchemePreference = window.localStorage.getItem(
@@ -10,21 +10,24 @@ const ThemeScriptTag = () => {
       )
       const hasPersistedPreference =
         typeof persistedColorSchemePreference === "string"
-    
+
       if (hasPersistedPreference && persistedColorSchemePreference !== "auto")
         return persistedColorSchemePreference
-    
+
       // if no localStorage value or value is 'auto' check media query
       const mql = window.matchMedia("(prefers-color-scheme: dark)")
       const hasMediaQueryPreference = typeof mql.matches === "boolean"
-    
+
       if (hasMediaQueryPreference) return mql.matches ? "dark" : "light"
-    
+
       // if browser doesn't support color themes set default to 'light'
       return "light"
     }
 
-    console.log(getInitialColorScheme);
+    console.log(getInitialColorScheme())
+    const colorMode = getInitialColorScheme()
+
+    document.body.setAttribute("data-theme", colorMode)
   })()`
 
   return <script dangerouslySetInnerHTML={{ __html: clientSideCode }} />
