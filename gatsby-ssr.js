858691e8402f969e9a5cd12b10f3aaf7d76a9c25
Jeff Winegar <winegarj@gmail.com>
2022-04-04 17:29:40 -0700

    fix: light theme flash on load in dark mode

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,31 @@
-const React = require("react")
-const Layout = require("./src/components/Layout").default
+import React from "react"
+import Layout from "./src/components/Layout"
 
-exports.wrapPageElement = ({ element, props }) => {
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
+const getInitialColorScheme = () => {
+  // Check for localStorage value
+  const persistedColorSchemePreference = window.localStorage.getItem(
+    "jw:setting:preferredColorScheme"
+  )
+  const hasPersistedPreference =
+    typeof persistedColorSchemePreference === "string"
+
+  if (hasPersistedPreference && persistedColorSchemePreference !== "auto")
+    return persistedColorSchemePreference
+
+  // if no localStorage value or value is 'auto' check media query
+  const mql = window.matchMedia("(prefers-color-scheme: dark)")
+  const hasMediaQueryPreference = typeof mql.matches === "boolean"
+
+  if (hasMediaQueryPreference) return mql.matches ? "dark" : "light"
+
+  // if browser doesn't support color themes set default to 'light'
+  return "light"
+}
+
+export const onRenderBody = ({ setBodyAttributes }) => {
+  setBodyAttributes({ "data-theme": getInitialColorScheme })
+}
+
+export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 }
